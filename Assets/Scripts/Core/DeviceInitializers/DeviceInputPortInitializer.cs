5842d9c9eb5004bb61999de5f7060898f448c138
stalengd <p.zamorskiy@yandex.ru>
2025-04-10 21:55:10 +0300

    Added device network gizmos

--- Assets/Scripts/Core/DeviceInitializers/DeviceInputPortInitializer.cs
+++ Assets/Scripts/Core/DeviceInitializers/DeviceInputPortInitializer.cs
@@ -9,5 +9,34 @@ namespace Perelesoq.TestAssignment.Core.DeviceInitializers
 
         public DeviceInputPort Port { get; set; }
         public DeviceOutputPortInitializer ConnectedTo => _connectedTo;
+
+        private void OnDrawGizmos()
+        {
+            if (_connectedTo == null)
+            {
+                Gizmos.color = new Color(1f, 0f, 0.2f, 0.7f);
+                Gizmos.DrawWireSphere(transform.position, 0.2f);
+                return;
+            }
+
+            var fromCenter = _connectedTo.transform.position;
+            var toCenter = transform.position;
+            var direction = toCenter - fromCenter;
+
+            Gizmos.color = new Color(1f, 0f, 1f, 0.5f);
+
+            Gizmos.DrawLine(fromCenter, toCenter);
+            DrawCone(toCenter, direction, 0.4f, 0.2f, 3);
+        }
+
+        private static void DrawCone(Vector3 tip, Vector3 direction, float length, float radius, int lines)
+        {
+            Vector3 basePoint = tip - direction.normalized * length;
+            for (var i = 0; i < lines; i++)
+            {
+                Gizmos.DrawLine(basePoint + Quaternion.AngleAxis(360f * i / lines, direction) * Vector3.up * radius,
+                    tip);
+            }
+        }
     }
 }
